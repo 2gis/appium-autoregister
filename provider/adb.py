56a970d00890fd32404fa8c2b67d63c957811814
s.zubov <s.zubov@2Gis.ru>
2017-06-20 14:48:23 +0700

    remove-unused-adb-commands

--- provider/adb.py
+++ provider/adb.py
@@ -1,8 +1,6 @@
 # coding: utf-8
 
-import os
 import sys
-import asyncio
 import logging
 
 from os import environ, path
@@ -12,39 +10,6 @@ from subprocess import Popen, PIPE
 ENCODING = sys.getdefaultencoding()
 
 log = logging.getLogger(__name__)
-
-
-async def adb_command(device, params, asynchronous=False):
-    assert device is not None
-
-    if isinstance(params, str):
-        params = params.split(" ")
-
-    android_home = os.environ.get("ANDROID_HOME", None)
-    if android_home is not None:
-        adb = os.path.join(android_home, 'platform-tools', 'adb')
-    else:
-        # if we don't have ANDROID_HOME set, let's just hope adb is in PATH
-        adb = 'adb'
-    command = [adb, '-s', device, '-H', 'localhost'] + params
-    p = await asyncio.create_subprocess_exec(
-        *command,
-        stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE,
-        env=os.environ.copy())
-    setattr(p, 'command', command)
-    if not asynchronous:
-        await p.wait()
-    return p
-
-
-async def until_adb_output(device_name, command, result):
-    _result = None
-    while _result != result:
-        _process = await adb_command(device_name, command)
-        _result = (await _process.stdout.read()).strip()
-
-    _process = await adb_command(device_name, command)
-    log.info("%s is %s" % (" ".join(_process.command), result))
 
 
 def get_command_output(p):
